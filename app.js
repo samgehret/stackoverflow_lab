2070539d2f01e642e4cabc74c5158ff58a71c0ed
Mahwash Mahr <mahwash.mahr@gmail.com>
2018-02-22 10:36:19 -0500

    handlebars working

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express ()
+const hbs = require('hbs')
+
+app.set('view engine', 'hbs')
 
 app.get('/', (req, res) => {
-    res.send('Hello world')
+    res.render('index')
 })
 
 
