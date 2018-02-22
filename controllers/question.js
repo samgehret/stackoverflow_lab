ca0c1cded93764ac8adaaa9d9d781c873798fce7
Sam Gehret <sam.gehret@gmail.com>
2018-02-22 12:33:01 -0500

    added show view for each question

--- controllers/question.js
+++ controllers/question.js
@@ -3,11 +3,18 @@ const router = express.Router()
 
 const Question = require('../models/Questions')
 
-router.get('/', (req,res)=>{
-    Question.find({})
+router.get('/', (req, res) => {
+  Question.find({})
         .then(questions => {
-            res.render('questions/index',{questions})
+          res.render('questions/index', {questions})
         })
+})
+
+router.get('/:id', (req, res) => {
+  Question.findOne({_id: req.params.id})
+    .then(questions => {
+      res.render('questions/show', questions)
+    })
 })
 
 module.exports = router
